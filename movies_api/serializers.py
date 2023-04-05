f8078a9640d964c07a73394aa8758c67f8cb0295
alex <alex@kali.alex.kali.local>
2023-04-05 20:06:38 -0300

    added ids to viewsets

--- movies_api/serializers.py
+++ movies_api/serializers.py
@@ -11,28 +11,28 @@ from letterboxd_scraper.models import (
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieModel
-        fields = ('title', 'slug', 'rating')
+        fields = ('id, title', 'slug', 'rating')
 
 
 class PopularReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = PopularReviewModel
-        fields = ('review_id', 'name', 'review', 'rating')
+        fields = ('id', 'review_id', 'name', 'review', 'rating')
 
 
 class RecentReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecentReviewModel
-        fields = ('review_id', 'name', 'review', 'rating')
+        fields = ('id', 'review_id', 'name', 'review', 'rating')
 
 
 class StatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = StatsModel
-        fields = ('views', 'likes', 'added_to_playlist')
+        fields = ('id', 'views', 'likes', 'added_to_playlist')
 
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImageModel
-        fields = ('release_year', 'poster_url', 'resized_poster')
+        fields = ('id', 'release_year', 'poster_url', 'resized_poster')
--- movies_api/views.py
+++ movies_api/views.py
@@ -18,36 +18,40 @@ from .serializers import (
 )
 
 
-class MoviePagination(pagination.PageNumberPagination):
-    page_size = 20
+class CommonPaginator(pagination.PageNumberPagination):
+    page_size = 100
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = MovieModel.objects.all()
     serializer_class = MovieSerializer
-    pagination_class = MoviePagination
+    pagination_class = CommonPaginator
     permission_classes = [permissions.AllowAny]
 
 
 class PopularViewSet(viewsets.ModelViewSet):
     queryset = PopularReviewModel.objects.all()
     permission_classes = [permissions.AllowAny]
+    pagination_class = CommonPaginator
     serializer_class = PopularReviewSerializer
 
 
 class RecentViewSet(viewsets.ModelViewSet):
     queryset = RecentReviewModel.objects.all()
     serializer_class = RecentReviewSerializer
+    pagination_class = CommonPaginator
     permission_classes = [permissions.AllowAny]
 
 
 class StatsViewSet(viewsets.ModelViewSet):
     queryset = StatsModel.objects.all()
     serializer_class = StatsSerializer
+    pagination_class = CommonPaginator
     permission_classes = [permissions.AllowAny]
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = ImageModel.objects.all()
     serializer_class = ImageSerializer
+    pagination_class = CommonPaginator
     permission_classes = [permissions.AllowAny]
