5d6541d1b73b6666776e4407c28a35df7a825a89
alex <alex@kali.alex.kali.local>
2023-04-08 03:21:56 -0300

    added

--- letterboxd_scraper/models.py
+++ letterboxd_scraper/models.py
@@ -5,29 +5,34 @@ class MovieModel(models.Model):
     title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
     rating = models.FloatField(null=True)
+    extraction_datetime = models.DateTimeField()
 
 
 class PopularReviewModel(models.Model):
     movie_id = models.CharField(null=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=800)
     review = models.TextField(max_length=800)
-    rating = models.CharField(max_length=255, null=True)
+    rating = models.CharField(max_length=800, null=True)
+    extraction_datetime = models.DateTimeField()
 
 
 class RecentReviewModel(models.Model):
     movie_id = models.CharField(null=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=800)
     review = models.TextField(max_length=800)
-    rating = models.CharField(max_length=255, null=True)
+    rating = models.CharField(max_length=800, null=True)
+    extraction_datetime = models.DateTimeField()
 
 
 class StatsModel(models.Model):
     views = models.IntegerField()
     likes = models.IntegerField()
     added_to_playlist = models.IntegerField()
+    extraction_datetime = models.DateTimeField()
 
 
 class ImageModel(models.Model):
     release_year = models.IntegerField(null=True)
-    poster_url = models.CharField(max_length=255, null=True)
-    resized_poster = models.CharField(max_length=255)
+    poster_url = models.CharField(max_length=800, null=True)
+    resized_poster = models.CharField(max_length=800)
+    extraction_datetime = models.DateTimeField()
